b7b9767837fecf0b934d0d765ed4a56e0cf991ab
chris <christoperjoel1@gmail.com>
2025-12-15 10:51:24 +0700

    Setup App Container (Part 2)

--- app/src/main/java/com/example/vp_alp_karep_frontend/AppContainer.kt
+++ app/src/main/java/com/example/vp_alp_karep_frontend/AppContainer.kt
@@ -10,7 +10,9 @@ class AppContainer (
         1. Buka cmd
         2. Ketik "ipconfig"
         3. Cari "IPv4 Address" pada bagian koneksi yang digunakan (Wi-Fi)
-        4. Ganti "192.168.x.xx" dengan IPv4 Address yang ditemukan
+        4. Ganti "192.168.x.xx" pada backendURL dengan IPv4 Address yang ditemukan
+        -- IP Address UC bisa beda beda jadi cari sendiri dan ganti sebelum presentasi! --
      */
     private val backendURL = "http://192.168.x.xx:3000"
+
 }
