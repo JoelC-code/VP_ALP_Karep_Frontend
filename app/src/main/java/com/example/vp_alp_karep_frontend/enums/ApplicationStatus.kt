86925a098110e58748f89397af66fb6c08f42082
chris <christoperjoel1@gmail.com>
2025-12-17 06:47:34 +0700

    ApplicationStatus for the + convert enums to strings

--- app/src/main/java/com/example/vp_alp_karep_frontend/enums/ApplicationStatus.kt
+++ app/src/main/java/com/example/vp_alp_karep_frontend/enums/ApplicationStatus.kt
@@ -1,8 +1,25 @@
 package com.example.vp_alp_karep_frontend.enums
 
+import com.google.gson.annotations.SerializedName
+
 enum class ApplicationStatus() {
+    @SerializedName("rejected")
     Rejected,
+
+    @SerializedName("accepted")
     Accepted,
+
+    @SerializedName("pending")
     Pending,
+
+    @SerializedName("cancelled")
     Cancelled,
 }
+
+fun ApplicationStatus.displayText(): String =
+    when(this) {
+        ApplicationStatus.Pending -> "Pending"
+        ApplicationStatus.Cancelled -> "Cancelled"
+        ApplicationStatus.Rejected -> "Rejected"
+        ApplicationStatus.Accepted -> "Accepted"
+    }
