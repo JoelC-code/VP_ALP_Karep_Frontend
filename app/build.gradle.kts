9223be19e645e72ffe298efe9783c4363ff3160e
chris <christoperjoel1@gmail.com>
2025-12-17 06:49:09 +0700

    Modification for gradle to make preview work

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.vp_alp_karep_frontend"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.vp_alp_karep_frontend"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
